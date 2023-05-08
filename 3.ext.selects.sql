585f3c1edd8ad7a6be6ec89a33eaec33c9c5e0cf
Кудрявцев Сергей <colorshoptmn@gmail.com>
2023-05-08 11:33:47 +0300

    reload

--- 3.ext.selects.sql
+++ 3.ext.selects.sql
@@ -1,20 +1,21 @@
-SELECT gen_id, COUNT(*) FROM genresinger
-GROUP BY gen_id
+SELECT genre_name, COUNT(*) FROM genre 
+JOIN genresinger ON genresinger.gen_id = genre.genre_id 
+GROUP BY genre.genre_name
 ORDER BY COUNT(*) DESC;
 
-SELECT album_id, COUNT(*) FROM track 
-WHERE release_date BETWEEN '2019-01-01' AND '2020-12-31'
-GROUP BY album_id 
-ORDER BY COUNT(*) DESC;
+SELECT COUNT(*) FROM track 
+JOIN album ON track.album_id = album.album_id 
+WHERE album.release_year BETWEEN '2019' AND '2020';
 
 SELECT album_id,AVG(duration_time) FROM track 
 GROUP BY album_id 
 ORDER BY AVG(duration_time);
 
-SELECT singer_name FROM singer 
-JOIN singeralbum ON singeralbum.s_id = singer.singer_id
-JOIN album ON singeralbum.alb_id = album.album_id 
-WHERE album.release_year != 2020;
+SELECT singer_name FROM singer
+WHERE singer_name NOT IN (SELECT singer_name FROM singer 
+JOIN singeralbum  ON singeralbum.s_id  = singer.singer_id
+JOIN album ON singeralbum.alb_id = album.album_id
+WHERE album.release_year = '2020');
 
 SELECT compilation_name FROM compilation
 JOIN comptrack ON comptrack.compil_id = compilation.compilation_id 
@@ -44,8 +45,9 @@ WHERE track.duration_time = (SELECT MIN(duration_time) FROM track);
 
 SELECT album_name FROM album 
 JOIN track ON track.album_id = album.album_id 
-GROUP BY album_name
-ORDER BY COUNT(*) ASC
-LIMIT 1;
-
-
+GROUP BY album.album_id
+HAVING COUNT(track.track_id) = 
+    (SELECT COUNT(track.track_id) FROM track
+    GROUP BY track.album_id 
+    ORDER BY 1
+    LIMIT 1);
